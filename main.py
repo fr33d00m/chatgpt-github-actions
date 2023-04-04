8a652c7f879f2e2a0432d6fe95b3fe3d2a374679
Vice Šimunov <vice.simunov@gmail.com>
2023-04-04 20:45:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,8 +18,6 @@ parser.add_argument('--openai_temperature', default=0.5,
                     help='Sampling temperature to use. Higher values means the model will take more risks. Recommended: 0.5')
 parser.add_argument('--openai_max_tokens', default=2048,
                     help='The maximum number of tokens to generate in the completion.')
-parser.add_argument('--mode', default="files",
-                    help='PR interpretation form. Options: files, patch')
 args = parser.parse_args()
 
 # Authenticating with the OpenAI API
@@ -33,93 +31,52 @@ def files():
     repo = g.get_repo(os.getenv('GITHUB_REPOSITORY'))
     pull_request = repo.get_pull(int(args.github_pr_id))
 
-    # Loop through the commits in the pull request
-    commits = pull_request.get_commits()
-    for commit in commits:
-        # Getting the modified files in the commit
-        files = commit.files
-        for file in files:
-            # Getting the file name and content
-            filename = file.filename
-            content = repo.get_contents(
-                filename, ref=commit.sha).decoded_content
+    # Define a file size threshold (in bytes) for sending only the diff
+  file_size_threshold = 6000  # Adjust this value as needed
 
-            # Sending the code to ChatGPT
-            response = openai.ChatCompletion.create(
-                model=args.openai_engine,
-                messages=[
-                    {"role": "system", "content": "You are a senior developer/architect and a helpful assistant."},
-                    {"role": "user", "content": f"Review this code patch and suggest improvements and issues:\n```{content}```"}
-                ],
-                temperature=float(args.openai_temperature),
-                max_tokens=int(args.openai_max_tokens)
-            )
+  # Process each file and its corresponding last commit SHA
+  for filename, sha in last_commit_shas.items():
+      # Getting the file content from the PR's last commit
+      file_pr = repo.get_contents(filename, ref=sha)
+      content_pr = file_pr.decoded_content
 
-            # Adding a comment to the pull request with ChatGPT's response
-            pull_request.create_issue_comment(
-              f"ChatGPT's response about `{file.filename}`:\n {response.choices[0].message.content}")
+      # Ignore binary 
+      
+      if file_pr.encoding == "base64":
+          continue
 
+      # Getting the file content from the main branch
+      file_main = repo.get_contents(filename, ref="main")
+      content_main = file_main.decoded_content
 
+      # Create a diff between the main branch and the PR's last commit
+      unified_diff = list(difflib.unified_diff(content_main.splitlines(), content_pr.splitlines()))
+      diff = "\n".join(unified_diff)
 
-def patch():
-    repo = g.get_repo(os.getenv('GITHUB_REPOSITORY'))
-    pull_request = repo.get_pull(int(args.github_pr_id))
+      # Get relevant context from the original content if the file size is below the threshold
+      context_lines = []
+      if len(content_main.encode('utf-8')) < file_size_threshold:
+          for line in unified_diff:
+              if line.startswith('+') and not line.startswith('+++'):
+                  context_lines.append(line[1:].strip())
 
-    content = get_content_patch()
+          context = "\n".join(context_lines)
+          user_message = f"Review this code patch and suggest improvements and issues:\n\nOriginal Context:\n```{context}```\n\nDiff:\n```{diff}```"
+      else:
+          user_message = f"Review this code patch and suggest improvements and issues:\n\nDiff:\n```{diff}```"
 
-    if len(content) == 0:
-        pull_request.create_issue_comment(
-            f"Patch file does not contain any changes")
-        return
+      # Sending the diff and context (if applicable) to ChatGPT
+      response = openai.ChatCompletion.create(
+          model=args.openai_engine,
+          messages=[
+              {"role": "system", "content": "You are a senior developer/architect and a helpful assistant."},
+              {"role": "user", "content": user_message}
+          ],
+          temperature=float(args.openai_temperature),
+          max_tokens=int(args.openai_max_tokens)
+      )
+      # Adding a comment to the pull request with ChatGPT's response
+      pull_request.create_issue_comment(
+        f"ChatGPT's response about `{file.filename}`:\n {response.choices[0].message.content}")
 
-    parsed_text = content.split("diff")
-
-    for diff_text in parsed_text:
-        if len(diff_text) == 0:
-            continue
-
-        try:
-            file_name = diff_text.split("b/")[1].splitlines()[0]
-            print(file_name)
-
-            response = openai.Completion.create(
-                engine=args.openai_engine,
-                prompt=(
-                    f"Summarize what was done in this diff:\n```{diff_text}```"),
-                temperature=float(args.openai_temperature),
-                max_tokens=int(args.openai_max_tokens)
-            )
-            print(response)
-            print(response['choices'][0]['text'])
-
-            pull_request.create_issue_comment(
-                f"ChatGPT's response about ``{file_name}``:\n {response['choices'][0]['text']}")
-        except Exception as e:
-            error_message = str(e)
-            print(error_message)
-            pull_request.create_issue_comment(
-                f"ChatGPT was unable to process the response about {file_name}")
-
-
-def get_content_patch():
-    url = f"https://api.github.com/repos/{os.getenv('GITHUB_REPOSITORY')}/pulls/{args.github_pr_id}"
-    print(url)
-
-    headers = {
-        'Authorization': f"token {args.github_token}",
-        'Accept': 'application/vnd.github.v3.diff'
-    }
-
-    response = requests.request("GET", url, headers=headers)
-
-    if response.status_code != 200:
-        raise Exception(response.text)
-
-    return response.text
-
-
-if (args.mode == "files"):
-    files()
-
-if (args.mode == "patch"):
-    patch()
+files()
