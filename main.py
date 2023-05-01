b48ca73058a08e5595736cc716753f2c9c2c42c8
fr33d00m <vice.simunov@gmail.com>
2023-05-01 12:18:30 +0200

    improve

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 import argparse
 import openai
 import os
+import tiktoken
+
 
 from github import Github
 
@@ -20,6 +22,8 @@ parser.add_argument('--openai_temperature', default=0.5,
 parser.add_argument('--openai_max_tokens', default=512,
                     help='The maximum number of tokens to generate in the completion.')
 args = parser.parse_args()
+
+max_file_tokens = 500;
 
 if not args.github_summary_token:
     args.github_summary_token = args.github_token
@@ -54,41 +58,9 @@ text_file_extensions = [
     '.cvsignore', '.svnignore', '.bzrignore', '.sol',
 ]
 
-def get_human_comments_since_last_review(pr_comments, filename, bot_username, last_review_timestamp):
-    human_comments = []
-
-    for comment in pr_comments:
-        if comment.user.login != bot_username and comment.path == filename and comment.created_at > last_review_timestamp:
-            human_comments.append(f"{comment.user.login} (line {comment.position}): {comment.body}")
-
-    return human_comments
-
-def find_previous_review_comment(pr_comments, filename, bot_username, search_for_exec_review=False):
-    previous_comment = None
-    previous_comment_timestamp = None
-    count = 0
-
-    # Sort the comments by their creation time
-    sorted_pr_comments = sorted(pr_comments, key=lambda comment: comment.created_at)
-
-    for comment in sorted_pr_comments:
-        if comment.user.login == bot_username:
-            if search_for_exec_review:
-                if "Executive Review" in comment.body:
-                    previous_comment = comment.body.split("Executive Review:")[-1].strip()
-            else:
-                if f"`{filename}`" in comment.body:
-                    previous_comment = comment.body
-
-
-    return previous_comment, count, previous_comment_timestamp
-
-
-def files():
+def main():
     # Authenticating with the Github API
     g = Github(args.github_token)
-    repo = g.get_repo(os.getenv('GITHUB_REPOSITORY'))
-    pull_request = repo.get_pull(int(args.github_pr_id))
 
     try:
         authenticated_user = g.get_user()
@@ -97,6 +69,8 @@ def files():
         print(f"Failed to get authenticated user's username, falling back to 'github-actions[bot]'")
         bot_username = "github-actions[bot]"
 
+    repo = g.get_repo(os.getenv('GITHUB_REPOSITORY'))
+    pull_request = repo.get_pull(int(args.github_pr_id))
     pr_comments = pull_request.get_issue_comments()
 
     engineering_feedback = []
@@ -104,8 +78,6 @@ def files():
     last_commit_shas = {}
     commits = pull_request.get_commits()
     final_files = pull_request.get_files();
-    print("PR file list:\n")
-    print("\n".join([file.filename for file in final_files]))
 
     for commit in commits:
         # Getting the modified files in the commit
@@ -121,97 +93,63 @@ def files():
             else:
                 last_commit_shas[file.filename] = {'sha': commit.sha, 'patch': file.patch}
 
-    # Define a file size threshold (in bytes) for sending only the diff
-    file_size_threshold = 6000  # Let's assume that 6k characters is too much for 2k tokens.
-
+    input_prompts = []
+    total_input_tokens = 0
     for filename, file_info in last_commit_shas.items():
-        sha = file_info['sha']
-        diff = file_info['patch']
-        print(f"Processing file: {filename}")
+        file_data = process_file(filename, file_info, repo, pr_comments, bot_username)
 
-        file_extension = os.path.splitext(filename)[1]
-        if file_extension not in text_file_extensions:
-            print(f"Skipping non-text file: {filename}")
+        if file_data is None:
             continue
 
-        # Getting the file content from the PR's last commit
-        file_pr = repo.get_contents(filename, ref=sha)
+        user_message = prepare_user_message(file_data)
+        user_message_tokens = count_tokens(user_message)
+        total_input_tokens += user_message_tokens
 
-        content_pr = file_pr.decoded_content.decode("utf-8")
+        input_prompts.append((filename, user_message))
 
-        if not diff:
-            print(f"No changes found in file: {filename}, skipping.")
+    if total_input_tokens > 3000:
+        print("### Large amount of content detected: Reviewing Only Diff")
+        single_user_message = "No wishy-washy shoulda-woulda-coulda, only actionable items. If the change is good write LGTM in the message. " \
+                              "Don't START the review with LGTM if you have insights or potential issues to share. " \
+                              "If your response starts with LGTM, summarize the change in 1-2 sentances." \
+                              "Keep any code you write to a minimum." \
+                              "This is the list of files and changes below them, no need to give an overall review. Review each file in order:"
+
+        for filename, file_info in last_commit_shas.items():
+            diff = file_info['patch']
+            single_user_message_part = prepare_single_review_all_files(diff, filename)
+            single_user_message += f"\n\n{single_user_message_part}"
+
+        input_prompts = [("### Large amount of content detected: Diff Review", single_user_message)]
+
+        # Recount the tokens
+        if count_tokens(single_user_message) > 6000:
+            print("!!!WARNING!!!: Too many tokens to process, skipping review.")
+            return
+
+    # Second loop: Process the prepared messages with ChatGPT
+    for filename, user_message in input_prompts:
+        gpt_response = engineering_gpt(user_message, args)
+
+        if gpt_response is None:
             continue
 
-        # Get relevant context from the original content if the file size is below the threshold
-        if len(content_pr) < file_size_threshold:
-            print(f"Sending context and diff for file: {filename}")
-            user_message = f"No wishy-washy shoulda-woulda-coulda, only actionable items. If the change is good write LGTM in the message. " \
-                           f"Don't START the review with LGTM if you have insights or potential issues to share." \
-                           f"The message you write when starting with LGTM will only be used in summary and not directly displayed to the programmer." \
-                           f"Keep any code you write to a minimum." \
-                           f"Review this code patch and suggest improvements and raise potential issues:\n\nLatest file Context:\n```{content_pr}```\n\nDiff from main:\n```{diff}```"
-        else:
-            print(f"Sending diff only for file: {filename}")
-            user_message = f"No wishy-washy shoulda-woulda-coulda, only actionable items. If the change is good write LGTM in the message. " \
-                           f"Don't START the review with LGTM if you have insights or potential issues to share. " \
-                           f"If your response starts with LGTM, summarize the change in 1-2 sentances." \
-                           f"The message you write when starting with LGTM will only be used in summary and not directly displayed to the programmer." \
-                           f"Keep any code you write to a minimum." \
-                           f"Review this code patch and suggest improvements and and raise potential issues:\n\nDiff:\n```{diff}```"
+        print(f"Received response from ChatGPT for file: {filename}")
+        gpt_responses.append(gpt_response)
 
-        previous_comment, review_count, previous_comment_timestamp = find_previous_review_comment(pr_comments, filename,
-                                                                                                  bot_username)
-        print(
-            f"For file {filename} found {review_count} review comments. Last timestamp: {previous_comment_timestamp} ")
-
-        last_commit = repo.get_commit(sha)
-        last_commit_timestamp = last_commit.commit.committer.date
-
-        human_comments = get_human_comments_since_last_review(pr_comments, filename, bot_username,
-                                                              previous_comment_timestamp)
-        if human_comments:
-            human_comments_str = "\n".join(human_comments)
-            user_message = f"{user_message}\n\n. Additionally, these are human reviewer comments on the pull request - are they addresed? \n\n{human_comments_str}"
-
-        # Check if the file hasn't been changed since the last review
-        if previous_comment_timestamp and last_commit_timestamp <= previous_comment_timestamp:
-            print(f"No updates found in file: {filename} since the last review, skipping.")
+        if gpt_response.strip().startswith("LGTM"):
             continue
 
-        if previous_comment:
-            print(f"Adjusting the message for previous review!!")
-            gpt_engineer_feedback = previous_comment.split(f"### `{filename}`:")[-1].split("### ")[0].strip()
-            user_message = f"You previously reviewed this code patch and suggested improvements and issues:\n\n{gpt_engineer_feedback}\n " \
-                           f"Changes were made, BE MORE concise than the last time. Were the comments addressed?  {user_message}"
+        engineering_feedback.append(f"### `{filename}`:\n"
+                                    f"{gpt_response}\n\n")
 
-        # Sending the diff and context (if applicable) to ChatGPT
-        try:
-            response = openai.ChatCompletion.create(
-                model=args.openai_engine,
-                messages=[
-                    {"role": "system", "content": "You are a senior developer & architect and a helpful assistant."},
-                    {"role": "user", "content": user_message}
-                ],
-                temperature=float(args.openai_temperature),
-                max_tokens=int(args.openai_max_tokens / 2)
-            )
-            print(f"Received response from ChatGPT for file: {filename}")
-            gpt_response = response.choices[0].message.content
-            gpt_responses.append(gpt_response)
+    all_responses = '\n'.join(gpt_responses)
 
-            if not gpt_response.strip().startswith("LGTM"):
-                engineering_feedback.append(f"### `{filename}`:\n"
-                                            f"{gpt_response}\n\n")
+    if count_tokens(all_responses > 1000):
+        all_responses = '\n'.join(engineering_feedback)
 
-        except Exception as e:
-            print(f"Error on GPT: {e}")
-
-    if len(gpt_responses) == 0:
-        return
-
-    max_len = 8000
-    all_responses = '\n'.join(gpt_responses)[:max_len]
+        if count_tokens(all_responses) > 1000:
+            return
 
     previous_exec_feedback, _, _ = find_previous_review_comment(pr_comments, "Executive Review", bot_username, True)
     if previous_exec_feedback:
@@ -259,4 +197,133 @@ def files():
         print(f"Error on GPT PR summary: {e}")
 
 
-files()
+
+def process_file(filename, file_info, repo, pr_comments, bot_username):
+    sha = file_info['sha']
+    diff = file_info['patch']
+    print(f"Processing file: {filename}")
+
+    file_extension = os.path.splitext(filename)[1]
+    if file_extension not in text_file_extensions:
+        print(f"Skipping non-text file: {filename}")
+        return None
+
+    file_pr = repo.get_contents(filename, ref=sha)
+    content_pr = file_pr.decoded_content.decode("utf-8")
+
+    if not diff:
+        print(f"No changes found in file: {filename}, skipping.")
+        return None
+
+    previous_comment, review_count, previous_comment_timestamp = find_previous_review_comment(pr_comments, filename, )
+
+    last_commit = repo.get_commit(sha)
+    last_commit_timestamp = last_commit.commit.committer.date
+
+    if previous_comment_timestamp and last_commit_timestamp <= previous_comment_timestamp:
+        print(f"No updates found in file: {filename} since the last review, skipping.")
+        return None
+
+    human_comments = get_human_comments_since_last_review(pr_comments, filename, bot_username, previous_comment_timestamp)
+    file_data = FileData(content_pr, diff, filename, previous_comment, previous_comment_timestamp, human_comments)
+    return file_data
+
+
+def prepare_user_message(content_pr, diff, filename, previous_comment,
+                         human_comments):
+    user_message = f"No wishy-washy shoulda-woulda-coulda, only actionable items. If the change is good write LGTM in the message. " \
+                       f"Don't START the review with LGTM if you have insights or potential issues to share." \
+                       f"The message you write when starting with LGTM will only be used in summary and not directly displayed to the programmer." \
+                       f"Keep any code you write to a minimum." \
+                       f"Review this code patch and suggest improvements and raise potential issues:\n\nLatest file Context:\n```{content_pr}```\n\nDiff from main:\n```{diff}```"
+
+    return append_previous_reviews(filename, human_comments, previous_comment, user_message)
+
+
+def append_previous_reviews(filename, human_comments, previous_comment, user_message):
+    if human_comments:
+        human_comments_str = "\n".join(human_comments)
+        user_message = f"{user_message}\n\n. Additionally, these are human reviewer comments on the pull request - are they addresed? \n\n{human_comments_str}"
+    if previous_comment:
+        gpt_engineer_feedback = previous_comment.split(f"### `{filename}`:")[-1].split("### ")[0].strip()
+        user_message = f"You previously reviewed this code patch and suggested improvements and issues:\n\n{gpt_engineer_feedback}\n " \
+                       f"Changes were made, BE MORE concise than the last time. Were the comments addressed?  {user_message}"
+    return user_message
+
+
+def prepare_single_review_all_files(diff, filename):
+    user_message = f"### `{filename}`:\n" \
+                   f"Diff:\n```{diff}```"
+    return user_message
+
+
+
+def engineering_gpt(user_message, engine):
+    try:
+        response = openai.ChatCompletion.create(
+            model=engine,
+            messages=[
+                {"role": "system", "content": "You are a senior developer & architect and a helpful assistant."},
+                {"role": "user", "content": user_message}
+            ],
+            temperature=float(args.openai_temperature),
+            max_tokens=int(args.openai_max_tokens / 2)
+        )
+        gpt_response = response.choices[0].message.content
+        return gpt_response
+
+    except Exception as e:
+        print(f"Error on GPT: {e}")
+        return None
+
+
+def get_human_comments_since_last_review(pr_comments, filename, bot_username, last_review_timestamp):
+    human_comments = []
+
+    for comment in pr_comments:
+        if comment.user.login != bot_username and comment.path == filename and comment.created_at > last_review_timestamp:
+            human_comments.append(f"{comment.user.login} (line {comment.position}): {comment.body}")
+
+    return human_comments
+
+
+def find_previous_review_comment(pr_comments, filename, bot_username, search_for_exec_review=False):
+    previous_comment = None
+    previous_comment_timestamp = None
+    count = 0
+
+    # Sort the comments by their creation time
+    sorted_pr_comments = sorted(pr_comments, key=lambda comment: comment.created_at)
+
+    for comment in sorted_pr_comments:
+        if comment.user.login == bot_username:
+            if search_for_exec_review:
+                if "Executive Review" in comment.body:
+                    previous_comment = comment.body.split("Executive Review:")[-1].strip()
+            else:
+                if f"`{filename}`" in comment.body:
+                    previous_comment = comment.body
+
+    return previous_comment, count, previous_comment_timestamp
+
+
+def count_tokens(text):
+    enc = tiktoken.encoding_for_model("gpt-4")
+    try:
+        tokens = enc.encode(text)
+        token_count = len(list(tokens))
+    except Exception:
+        token_count = 0
+    return token_count
+
+
+class FileData:
+    def __init__(self, content_pr, diff, filename, previous_comment, human_comments):
+        self.content_pr = content_pr
+        self.diff = diff
+        self.filename = filename
+        self.previous_comment = previous_comment
+        self.human_comments = human_comments
+
+
+main()
